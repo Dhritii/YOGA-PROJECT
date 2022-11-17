12e2c5a1d5cb0c6134def2188416a41fe8a0e2d6
Dhritii <90952624+Dhritii@users.noreply.github.com>
2022-11-17 21:38:32 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,9 @@
+//NAVBAR
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+
